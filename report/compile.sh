1407d3538edd33380e3df4c49927784265a8b9ea
Maëlick Claes <himself@maelick.net>
2011-05-29 17:13:44 +0200

    Report: Chapter 3 + 4 updated + images updated + bibliography completion

--- report/compile.sh
+++ report/compile.sh
@@ -1,13 +1,8 @@
-convert img/fig_clientloc.png fig_clientloc.eps
-convert img/fig_copypaste.png fig_copypaste.eps
-convert img/fig_join.png fig_join.eps
-convert img/fig_local.png fig_local.eps
 convert img/fig_p2p.png fig_p2p.eps
 convert img/umons.png umons.eps
-convert img/p2pclient_broadcast.png p2pclient_broadcast.eps
-convert img/p2pclient_connections.png p2pclient_connections.eps
-convert img/p2pclient_peerhandler1.png p2pclient_peerhandler1.eps
-convert img/p2pclient_peerhandler2.png p2pclient_peerhandler2.eps
+convert img/broadcast.png broadcast.eps
+convert img/connections.png connections.eps
+convert img/peerhandler.png peerhandler.eps
 latex report.tex
 bibtex report.aux
 latex report.tex
